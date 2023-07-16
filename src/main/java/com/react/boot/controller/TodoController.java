d0324acdb75cc85925ed75fc243a08b8082794fe
SUJINJEONG012 <peekaboo32@naver.com>
2023-07-16 22:09:47 +0900

    CORS  config 파일 수정

--- src/main/java/com/react/boot/controller/TodoController.java
+++ src/main/java/com/react/boot/controller/TodoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.react.boot.dto.TodoDTO;
 import com.react.boot.entity.TodoEntity;
 import com.react.boot.service.TodoService;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("todo")
 public class TodoController {
